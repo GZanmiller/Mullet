22681a54f17631f46d52328fb7e72b408a93a8a0
Greg <Rcfan0902@aol.com>
2013-02-12 12:38:26 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mullete::Application.config.secret_token = '51e298abcb9408695f4654e4e45e1dc07322eabacae2c44636680418b0ae8a9863969233ab61081beeb7bf7ea573fcad206a81f1949e974e2f9d2ae24819a94c'
